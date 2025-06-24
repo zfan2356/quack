a6dd13102ae608a1aea722cd0a369900f3d22826
Tri Dao <tridpq@gmail.com>
2025-06-23 22:53:12 -0400

    Bump to v0.1.3

--- quack/__init__.py
+++ quack/__init__.py
@@ -1,5 +1,11 @@
-__version__ = "0.1.2"
+__version__ = "0.1.3"
 
 from quack.rmsnorm import rmsnorm
 from quack.softmax import softmax
 from quack.cross_entropy import cross_entropy
+
+__all__ = [
+    "rmsnorm",
+    "softmax",
+    "cross_entropy",
+]
