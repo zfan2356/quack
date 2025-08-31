3adfae1000a0c89606402274771f57d907fcf93c
Tri Dao <tridpq@gmail.com>
2025-08-30 22:20:45 -0400

    [Chore] Enable ruff-format again

--- quack/cute_dsl_utils.py
+++ quack/cute_dsl_utils.py
@@ -1,6 +1,7 @@
 import os
 import pathlib
 from functools import partial
+
 try:
     from triton.tools.disasm import extract
 except ImportError:
@@ -21,8 +22,7 @@ def load_cubin_module_data_patched(cubin_data, filepath):
 
 
 def cute_compile_patched(*args, **kwargs):
-    """A patched version of cute.compile that dump the SASS to a file if CUTE_CUBIN_PATH is set.
-    """
+    """A patched version of cute.compile that dump the SASS to a file if CUTE_CUBIN_PATH is set."""
     if os.getenv("CUTE_CUBIN_PATH") is not None:
         cutlass.base_dsl.runtime.cuda.load_cubin_module_data = partial(
             load_cubin_module_data_patched, filepath=os.getenv("CUTE_CUBIN_PATH")
